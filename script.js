4ae7374a8a8f67984502702f7509cda5f081d432
Hirad <hiraddevcomp@gmail.com>
2025-08-12 23:53:36 -0700

    Hirad push, update courses page, Bug fixed

--- script.js
+++ script.js
@@ -1,23 +1,3 @@
-// Smooth scroll for navigation links
-document.querySelectorAll('header a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        const targetId = this.getAttribute('href');
-        const targetSection = document.querySelector(targetId);
-        
-        if (targetSection) {
-            const headerHeight = document.querySelector('header').offsetHeight;
-            const targetPosition = targetSection.offsetTop - headerHeight - 20;
-            
-            window.scrollTo({
-                top: targetPosition,
-                behavior: 'smooth',
-                duration: 1000
-            });
-        }
-    });
-});
-
 // Scroll animation for sections
 const sections = document.querySelectorAll("section");
 
